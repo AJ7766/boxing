a456a0769f0ae057cbc0c3633c4a459cd0863b64
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-01-26 21:56:18 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import { DateTime } from 'luxon';
 
 export default function Home() {
-  const date = new Date();
+  const date = DateTime.local();
+  const dateNow = DateTime.now();
 
-  const localTimeFormat = (date: Date) => {
-    const localDateTime = DateTime.fromJSDate(date).setZone('local', { keepLocalTime: true }) ;
+  const localTimeFormat = (date: DateTime) => {
+    const localDateTime = date.setZone('local', { keepLocalTime: true }) ;
     return localDateTime.toFormat('HH:mm - dd/MM/yyyy')
   }
 
   return (
     <>
-      <div>LOCAL TIME2: {localTimeFormat(date)}</div>
+      <div>DateTime Now: {dateNow.toFormat('HH:mm - dd/MM/yyyy')}</div>
+      <div>DateTime Local: {localTimeFormat(date)}</div>
     </>
   );
 }
