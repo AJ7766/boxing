83305399477ab8a7b9f15fcdd62c5e5466a1adf0
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-01-27 14:23:42 +0100

    Update Time.tsx

--- components/Time.tsx
+++ components/Time.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { DateTime } from "luxon";
 import { useLayoutEffect, useState } from "react";
 
 export const Time = () => {
@@ -9,24 +10,11 @@ export const Time = () => {
         setIsClient(true);
     }, [])
 
-    return <div>LOCAL TIME: {isClient ? localTimeFormat2(date) : 'Loading...'}</div>
+    return <div>LOCAL TIME: {isClient ? localTimeFormat(date) : 'Loading...'}</div>
 }
 
-/* const localTimeFormat = (date: Date) => {
+const localTimeFormat = (date: Date) => {
     const localDateTime = DateTime.fromJSDate(date).setZone('local');
-    return localDateTime.toFormat('HH:mm - dd/MM/yyyy')
-}
- */
-const localTimeFormat2 = (date: Date) => {
-    const localDateTime = date.toLocaleString('default', {
-        hour: '2-digit',
-        minute: '2-digit',
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-    });
-    const [hours, minutes] = localDateTime.split(' ')[1].split(':');
-    const [year, month, day] = localDateTime.split(' ')[0].split('-');
-
-    return `${hours}:${minutes} - ${day}/${month}/${year}`;
+    const timeZone = localDateTime.zoneName;
+    return localDateTime.toFormat('HH:mm - dd/MM/yyyy') + ' - ' + timeZone;
 }
