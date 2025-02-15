8edee4bb5bb8ad55c4ad82313652a493cc5c0140
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-02-15 14:49:36 +0100

    Update Table.tsx

--- components/Rankings/Table.tsx
+++ components/Rankings/Table.tsx
@@ -29,7 +29,7 @@ export const Table = ({ mensRankings, womensRankings }: { mensRankings: Rankings
 
     useEffect(() => {
         handleSort('theRing');
-    }, [])
+    }, [handleSort])
 
     return (
         <>
