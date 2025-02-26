081e6ee802ba718849de825f83a4092d43fc7af1
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-02-26 23:16:26 +0100

    test

--- components/Nav/Link.tsx
+++ components/Nav/Link.tsx
@@ -1,16 +1,20 @@
 import { rajdhani } from "@/fonts/fonts";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { ForwardedRef } from "react";
+import { ForwardedRef, useEffect } from "react";
 
 export const LinkC = ({ name, ref }: { name: string, ref: ForwardedRef<HTMLAnchorElement> }) => {
     const router = useRouter();
     const href = `/${name === 'news' ? '/' : name.toLocaleLowerCase()}`;
+
     return <li className={`${rajdhani.className} font-semibold text-base`}>
         <Link
             ref={ref}
             prefetch={false}
             href={href}
+            onClick={() => window.scrollTo(0, 0)} // Scroll to top when clicked
+
+            // Prefetching the hovered link for faster page loading
             onMouseEnter={() => router.prefetch(href)}
         >
             {name.toUpperCase()}
