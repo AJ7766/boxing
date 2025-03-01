ffb09765153467762845c04e35fa8eb8af4ad63b
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-03-01 17:48:03 +0100

    Update Table.tsx

--- components/Rankings/Table.tsx
+++ components/Rankings/Table.tsx
@@ -4,50 +4,79 @@ import React, { useCallback, useEffect, useState } from 'react'
 
 export const Table = ({ mensRankings, womensRankings }: { mensRankings: RankingsProps[], womensRankings: RankingsProps[] }) => {
     const [womens, setWomens] = useState(false);
-    const [sortedRankings, setSortedRankings] = useState<RankingsProps[]>(mensRankings);
+    const [sortedRankings, setSortedRankings] = useState({
+        ranking: mensRankings as RankingsProps[],
+        selectedRanking: 'theRing'
+    });
 
     useEffect(() => {
-        setSortedRankings(womens ? womensRankings : mensRankings);
+        setSortedRankings({
+            ranking: womens ? womensRankings : mensRankings,
+            selectedRanking: 'theRing'
+        });
     }, [womens, mensRankings, womensRankings]);
 
     const handleSort = useCallback((key: keyof RankingsProps) => {
-        setSortedRankings(prevRankings => {
-            const sorted = [...prevRankings].sort((a, b) => {
+        console.log("Sorting by", key);
+        setSortedRankings(() => {
+            const sorted = sortedRankings.ranking.sort((a, b) => {
                 const aValue = Number(a[key]?.match(/(\d+)/)?.[0]) || Infinity;
                 const bValue = Number(b[key]?.match(/(\d+)/)?.[0]) || Infinity;
 
                 return aValue - bValue;
             });
-            return sorted;
+            return {
+                ranking: sorted,
+                selectedRanking: key
+            };
         });
     }, []);
+
+    const pseudoActiveClass =
+        "after:absolute after:bottom-2 after:left-1/2 after:-translate-x-1/2 after:w-1/4 after:h-[1.7px] after:bg-red-600 after:transition-[opacity] after:opacity-100 after:duration-300";
 
     return (
         <>
             <div className='flex text-2xl font-medium justify-center mt-10'>
-                <button className={`px-3 py-2 ${!womens && 'bg-gray-200 rounded'}`} onClick={() => setWomens(false)}>Men&apos;s</button>
-                <button className={`px-3 ${womens && 'bg-gray-200 rounded'}`} onClick={() => setWomens(true)}>Women&apos;s</button>
+                <button className={`relative px-3 py-2 
+    after:content-[''] after:absolute after:bottom-2 after:h-[3px] after:left-1/2 after:-translate-x-1/2 after:bg-red-600 after:transition-[width] after:duration-300 
+    ${!womens ? 'after:w-2/5' : 'after:w-0 hover:after:w-[calc(100%-24px)]'}`} onClick={() => setWomens(false)}>Men&apos;s</button>
+                <button className={`relative px-3 py-2 
+    after:content-[''] after:absolute after:bottom-2 after:h-[3px] after:left-1/2 after:-translate-x-1/2 after:bg-red-600 after:transition-[width] after:duration-300 
+    ${womens ? 'after:w-2/5' : 'after:w-0 hover:after:w-[calc(100%-24px)]'}`} onClick={() => setWomens(true)}>Women&apos;s</button>
             </div>
             <div className='relative mt-6 overflow-x-auto'>
                 <table className='mx-auto font-medium table-collapse'>
                     <thead>
                         <tr>
-                            <TableHeader rowSpan={2}>Test</TableHeader>
+                            <TableHeader rowSpan={2}>Boxer</TableHeader>
                             <TableHeader rowSpan={2}>Record</TableHeader>
                             <TableHeader rowSpan={2}>Weight Class</TableHeader>
                             <TableHeader rowSpan={2}>Current World Titles</TableHeader>
                             <TableHeader colSpan={womens ? 3 : 5}>Ranker</TableHeader>
                         </tr>
                         <tr className="border-b-2 border-gray-500 cursor-pointer">
-                            <TableHeader onClick={() => handleSort('theRing')}>The Ring</TableHeader>
-                            {!womens && <TableHeader onClick={() => handleSort('bwaa')}>BWAA</TableHeader>}
-                            {!womens && <TableHeader onClick={() => handleSort('tbrb')}>TBRB</TableHeader>}
-                            <TableHeader onClick={() => handleSort('espn')}>ESPN</TableHeader>
-                            <TableHeader onClick={() => handleSort('boxRec')}>BoxRec</TableHeader>
+                            <TableHeader
+                                className={`${sortedRankings.selectedRanking === 'theRing' && pseudoActiveClass}`}
+                                onClick={() => handleSort('theRing')}>The Ring</TableHeader>
+                            {!womens && <>
+                                <TableHeader
+                                    className={`${sortedRankings.selectedRanking === 'bwaa' && pseudoActiveClass}`}
+                                    onClick={() => handleSort('bwaa')}>BWAA</TableHeader>
+                                <TableHeader
+                                    className={`${sortedRankings.selectedRanking === 'tbrb' && pseudoActiveClass}`}
+                                    onClick={() => handleSort('tbrb')}>TBRB</TableHeader>
+                            </>}
+                            <TableHeader
+                                className={`${sortedRankings.selectedRanking === 'espn' && pseudoActiveClass}`}
+                                onClick={() => handleSort('espn')}>ESPN</TableHeader>
+                            <TableHeader
+                                className={`${sortedRankings.selectedRanking === 'boxRec' && pseudoActiveClass}`}
+                                onClick={() => handleSort('boxRec')}>BoxRec</TableHeader>
                         </tr>
                     </thead>
                     <tbody>
-                        {sortedRankings.map((ranking) => (
+                        {sortedRankings.ranking.map((ranking) => (
                             <tr key={ranking.id} className="border-b border-gray-300">
                                 <TableCell>{ranking.boxer}</TableCell>
                                 <TableCell>{ranking.record}</TableCell>
@@ -71,15 +100,15 @@ export const Table = ({ mensRankings, womensRankings }: { mensRankings: Rankings
                             </tr>
                         ))}
                     </tbody>
-                </table>
-            </div>
+                </table >
+            </div >
         </>
     )
 }
 
-const TableHeader = ({ children, rowSpan, colSpan, onClick }: { children: React.ReactNode, rowSpan?: number, colSpan?: number, onClick?: () => void }) => (
+const TableHeader = ({ children, className, rowSpan, colSpan, onClick }: { children: React.ReactNode, className?: string, rowSpan?: number, colSpan?: number, onClick?: () => void }) => (
     <th
-        className={`px-4 py-2 border border-gray-300 ${onClick && 'cursor-pointer'}`}
+        className={`relative px-4 py-2 border border-gray-300 ${onClick && 'cursor-pointer'} after:opacity-0 ${className}`}
         onClick={onClick}
         rowSpan={rowSpan}
         colSpan={colSpan}
