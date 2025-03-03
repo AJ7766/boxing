d0ba50ec252dd26aa76131bd5632399d31a439cf
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-03-03 16:43:43 +0100

    ,

--- utils/DateTimeFormatter.tsx
+++
@@ -1,9 +0,0 @@
-import { DateTime } from "luxon";
-
-export const localTimeFormat = (date?: string | null) => {
-    if (date === null || date === undefined) return 'No time available';
-    const localDateTime = DateTime.fromISO(date).setZone('local');
-    const timeZone = localDateTime.zoneName;
-
-    return `${localDateTime.toFormat('ccc')}, ${localDateTime.toFormat('dd MMMM - (HH:mm')} ${timeZone})`
-}
