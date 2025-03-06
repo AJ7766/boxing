321d53744045565b850834c02512e69e3fa341f7
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-03-06 19:48:12 +0100

    Update Hero.tsx

--- components/News/Hero.tsx
+++ components/News/Hero.tsx
@@ -9,21 +9,19 @@ import { useIsClient } from "@/hooks/useClient";
 gsap.registerPlugin(ScrollTrigger);
 
 export const Hero = () => {
-    const heroRef = useRef<HTMLElement>(null);
     const isClient = useIsClient();
+    const heroRef = useRef<HTMLElement>(null);
+    
     useGSAP(() => {
         if (!heroRef.current || !isClient) return;
-        gsap.to(heroRef.current, { backgroundColor: "#f7fe19", duration: 0.5, ease: "power3.in" });
 
-        gsap.fromTo(
+        gsap.to(
             heroRef.current,
-            { opacity: 1 },
-            {
-                opacity: 0.3,
+            { backgroundColor: "black", opacity: 0.85,
                 scrollTrigger: {
                     trigger: heroRef.current,
                     start: "75% 50%",
-                    end: "bottom 20%",
+                    end: "bottom top",
                     scrub: true,
                 },
             }
@@ -32,7 +30,7 @@ export const Hero = () => {
 
     return (
         <div className="h-screen w-full">
-            <section ref={heroRef} className={`${dazn.className} fixed h-full w-full bg-black font-semibold text-[8.5vw] leading-[1.1] flex items-center flex-col justify-center -z-10`}>
+            <section ref={heroRef} className={`${dazn.className} fixed h-full w-full bg-[#f7fe19] font-semibold text-[8.5vw] leading-[1.1] flex items-center flex-col justify-center -z-10`}>
                 <h1>RYAN GARCIA</h1>
                 <h1>VS</h1>
                 <h1>ROLLY ROMERO</h1>
