97f91c47cc449fcc30f2004af7bcc635256e8c42
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-03-21 19:44:28 +0100

    Update FightersTable.tsx

--- components/Fights/FightersTable.tsx
+++ components/Fights/FightersTable.tsx
@@ -1,10 +1,10 @@
 export const FightersTable = ({ fighter1, stats1, fighter2, stats2 }: { fighter1?: string | null, stats1?: string | null, fighter2?: string | null, stats2?: string | null }) => {
     return (
         <div className="h-full flex flex-col justify-between items-start">
-            <table className="border-collapse">
+            <table>
                 <tbody>
                     <tr className="relative text-xl font-bold leading-none
-                    before:w-full before:absolute before:bottom-0 before:left-0 before:h-[3px] 
+                    before:w-full before:absolute before:-bottom-[0.5px] before:left-0 before:h-[3px] 
                     before:bg-gradient-to-r before:from-red-600 before:to-blue-black before:to-blue-500 before:animate-pulse">
                         <td className="pr-4 inline-block">{fighter1}</td>
                         <td className="pr-4">VS</td>
